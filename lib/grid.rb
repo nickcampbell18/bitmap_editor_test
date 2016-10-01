8d88d094fe05e316d3184fbde1924d13f5ed247a
Nick Campbell <ncampbell@yammer-inc.com>
2016-10-01 10:49:05 +0100

    Implement Grid striping and printing
    Notice the two stripe methods take the row/col
    first, and then the two parameters. I felt this
    was more consistent internally. The external
    consumer of this code will switch these params around
    before calling the Grid class.
    
    Also added a bunch of input validations. I decided
    to put them here and prevent bad values descending
    further into the stack. We can catch ArgumentError
    at the boundary (bitmap_editor.rb) and show a pretty
    error to the user at that point.

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,10 +1,13 @@
 class Grid
 
   WHITE_PIXEL = 'O'.freeze
+  COLOUR_RANGE = (?A..?Z).freeze
+  GRID_RANGE = (1..250).freeze
 
   def initialize(cols, rows)
     @rows = rows.to_i.freeze
     @cols = cols.to_i.freeze
+    check_dimensions(@cols, @rows)
     @grid = Array.new(@rows * @cols)
     clear
   end
@@ -14,20 +17,68 @@ class Grid
   end
   alias_method :[], :read
 
-  def colour(col, row, colour)
+  def write(col, row, colour)
+    check_colour(colour)
     cell = translate(col, row)
     @grid[cell] = colour
   end
+  alias_method :[]=, :write
 
   def clear
     @grid.map! { WHITE_PIXEL }
   end
 
+  def vertical_stripe(column, top_row, bottom_row, colour)
+    row_range = Range.new(*[top_row, bottom_row].sort)
+    row_range.each do |row|
+      write(column, row, colour)
+    end
+  end
+
+  def horizontal_stripe(row, left_column, right_column, colour)
+    col_range = Range.new(*[left_column, right_column].sort)
+    col_range.each do |col|
+      write(col, row, colour)
+    end
+  end
+
+  def print
+    @grid
+      .each_slice(@cols)
+      .map { |row| row.join('') }
+      .join("\n")
+  end
+  alias_method :to_s, :print
+
   private
 
   def translate(col, row)
+    check_range(col, row)
     x = col - 1
     y = row - 1
     x * @cols + y
+  end
+
+  def check_range(col, row)
+    if col > @cols || col < 1
+      raise ArgumentError, "Column #{col} is out of range (#{@cols} columns available)"
+    elsif row > @rows || row < 1
+      raise ArgumentError, "Row #{row} is out of range (#{@rows} rows available)"
+    end
+  end
+
+  def check_dimensions(given_cols, given_rows)
+    if !GRID_RANGE.cover?(given_rows)
+      raise ArgumentError, "Grid height must be between 1 and 250, but given #{given_rows}"
+    elsif !GRID_RANGE.cover?(given_cols)
+      raise ArgumentError, "Grid width must be between 1 and 250, but given #{given_cols}"
+    end
+  end
+
+  def check_colour(colour)
+    # Assumption: colours are ASCII upper-alpha only
+    unless COLOUR_RANGE.cover?(colour)
+      raise ArgumentError, "Colour #{colour} is not an uppercase A-Z character"
+    end
   end
 end
--- test/grid_test.rb
+++ test/grid_test.rb
@@ -3,6 +3,8 @@ require_relative '../lib/grid.rb'
 
 describe Grid do
 
+  INVALID_COLOURS = [0, nil, 'a', 'Ś'].freeze
+
   describe '#initialize' do
     it 'should construct a 2x2 grid full of WHITE' do
       grid = Grid.new(2, 2)
@@ -11,30 +13,171 @@ describe Grid do
       assert_equal 'O', grid[2, 1]
       assert_equal 'O', grid[2, 2]
     end
+
+    it 'should raise an ArgumentError if the row number is not between 1 and 250' do
+      [-1, 0, 251].each do |bad_row|
+        assert_raises(ArgumentError) { Grid.new(5, bad_row) }
+      end
+    end
+
+    it 'should raise an ArgumentError if the column number is not between 1 and 250' do
+      [-1, 0, 251].each do |bad_col|
+        assert_raises(ArgumentError) { Grid.new(bad_col, 4) }
+      end
+    end
   end
 
-  describe '#colour' do
+  describe '#[] (read)' do
+    it 'should not overflow if given column is out of range' do
+      grid = Grid.new(3, 3)
+      err = assert_raises(ArgumentError) do
+        grid[4, 3]
+      end
+      assert_equal 'Column 4 is out of range (3 columns available)', err.message
+    end
+
+    it 'should not overflow if given row is out of range' do
+      grid = Grid.new(3, 3)
+      err = assert_raises(ArgumentError) do
+        grid[3, 0]
+      end
+      assert_equal 'Row 0 is out of range (3 rows available)', err.message
+    end
+
+    it 'should not overflow on negative rows' do
+      grid = Grid.new(2, 2)
+      assert_raises(ArgumentError) do
+        grid[2, -1]
+      end
+    end
+  end
+
+  describe '#write' do
     before do
       @grid = Grid.new(3, 3)
     end
 
     it 'should colour the first cell' do
-      @grid.colour(1, 1, 'F')
+      @grid.write(1, 1, 'F')
       assert_equal 'F', @grid[1, 1]
     end
 
     it 'should colour the middle cell' do
-      @grid.colour(2, 2, 'G')
+      @grid.write(2, 2, 'G')
       assert_equal 'G', @grid[2, 2]
+    end
+
+    it 'should not colour other cells' do
+      @grid.write(3, 3, 'X')
+      white_cells = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2]]
+      white_cells.each do |(col, row)|
+        assert_equal 'O', @grid[col, row]
+      end
+    end
+
+    it 'also accepts the shorthand []=' do
+      @grid[1, 1] = 'X'
+      assert_equal 'X', @grid[1, 1]
+    end
+
+    it 'should raise an error if given an invalid colour' do
+      INVALID_COLOURS.each do |bad_colour|
+        assert_raises(ArgumentError) { @grid.write(1, 1, bad_colour) }
+      end
+    end
+  end
+
+  describe '#vertical_stripe' do
+    before do
+      @grid = Grid.new(5, 5)
+    end
+
+    it 'should paint the central cell' do
+      @grid.vertical_stripe(3, 3, 3, 'R')
+      assert_equal 'R', @grid[3, 3]
+      # None of the surrounding cells
+      assert_equal 'O', @grid[3, 2]
+      assert_equal 'O', @grid[3, 4]
+      assert_equal 'O', @grid[2, 3]
+      assert_equal 'O', @grid[2, 4]
+    end
+
+    it 'should paint a stripe along the whole height' do
+      @grid.vertical_stripe(2, 1, 5, 'T')
+      [[2, 1], [2, 2], [2, 3], [2, 4], [2, 5]].each do |(col, row)|
+        assert_equal 'T', @grid[col, row]
+      end
+    end
+
+    it 'should raise an error if given an invalid colour' do
+      INVALID_COLOURS.each do |bad_colour|
+        assert_raises(ArgumentError) { @grid.vertical_stripe(1, 1, 1, bad_colour) }
+      end
+    end
+  end
+
+  describe '#horizontal_stripe' do
+    before do
+      @grid = Grid.new(5, 5)
+    end
+
+    it 'should paint the central cell' do
+      @grid.horizontal_stripe(3, 3, 3, 'R')
+      assert_equal 'R', @grid[3, 3]
+      # None of the surrounding cells
+      assert_equal 'O', @grid[3, 2]
+      assert_equal 'O', @grid[3, 4]
+      assert_equal 'O', @grid[2, 3]
+      assert_equal 'O', @grid[2, 4]
+    end
+
+    it 'should paint a stripe along the whole width' do
+      @grid.horizontal_stripe(1, 1, 5, 'T')
+      [[1, 1], [2, 1], [3, 1], [4, 1], [5, 1]].each do |(col, row)|
+        assert_equal 'T', @grid[col, row]
+      end
+    end
+
+    it 'should be able to paint backwards' do
+      @grid.horizontal_stripe(5, 4, 2, 'W')
+      [[2, 5], [3, 5], [4, 5]].each do |(col, row)|
+        assert_equal 'W', @grid[col, row]
+      end
+    end
+
+    it 'should raise an error if given an invalid colour' do
+      INVALID_COLOURS.each do |bad_colour|
+        assert_raises(ArgumentError) { @grid.horizontal_stripe(1, 1, 1, bad_colour) }
+      end
     end
   end
 
   describe '#clear' do
     it 'should reset the colour state to WHITE' do
       grid = Grid.new(2, 2)
-      grid.colour(1, 1, 'X')
+      grid.write(1, 1, 'X')
       grid.clear
       assert_equal 'O', grid[1, 1]
+    end
+  end
+
+  describe '#print' do
+    it 'should print a simple grid' do
+      grid = Grid.new(2, 2)
+      assert_equal "OO\nOO", grid.print
+    end
+
+    it 'should print a complex grid' do
+      grid = Grid.new(3, 3)
+      grid[1, 3] = 'D'
+      grid[2, 2] = 'R'
+      grid[3, 3] = 'E'
+      assert_equal "OOD\nORO\nOOE", grid.print
+    end
+
+    it 'also accepts the shorthand #to_s' do
+      grid = Grid.new(1, 1)
+      assert_equal 'O', grid.to_s
     end
   end
 
